35835716a1079bcca29dfb8e0bbfe1bcda1cf544
Kouji Matsui <k@kekyo.net>
2024-04-12 17:21:20 +0900

    Improved tests.

--- samples/DupeNukem.WebView2/ViewModels/TestFragments.cs
+++ samples/DupeNukem.WebView2/ViewModels/TestFragments.cs
@@ -10,7 +10,10 @@
 ////////////////////////////////////////////////////////////////////////////
 
 using System;
+using System.Collections;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -70,10 +73,10 @@ partial class MainWindowViewModel
         messenger.RegisterFunc<ConsoleKey[], ConsoleKey[]>(
             "array",
             async keys => { await Task.Delay(100); return keys; });
-        messenger.RegisterFunc<string, int, int, Func<int, int, Task<string>>>(
+        messenger.RegisterFunc<int, int, int, Func<int, int, Task<int>>>(
             "callback",
             async (a, b, cb) => { var r = await cb(a, b); return r; });
-        messenger.RegisterFunc<string, int, int, Func<int, int, CancellationToken, Task<string>>>(
+        messenger.RegisterFunc<int, int, int, Func<int, int, CancellationToken, Task<int>>>(
             "callback2",
             async (a, b, cb) => { var r = await cb(a, b, default); return r; });
     }
@@ -86,6 +89,23 @@ partial class MainWindowViewModel
     public async Task<int> FromEnum(ConsoleKey key) { await Task.Delay(100); return (int)key; }
     public async Task<ConsoleKey> ToEnum(int key) { await Task.Delay(100); return (ConsoleKey)key; }
     public async Task<ConsoleKey[]> Array(ConsoleKey[] keys) { await Task.Delay(100); return keys; }
+
+    private static void Assert(object expected, object actual, string message)
+    {
+        if (expected.Equals(actual))
+        {
+            Trace.WriteLine($"PASSED: {message}");
+        }
+        else if (expected is IEnumerable ex && actual is IEnumerable ac &&
+            ex.Cast<object>().SequenceEqual(ac.Cast<object>()))
+        {
+            Trace.WriteLine($"PASSED: {message}");
+        }
+        else
+        {
+            Trace.Fail($"FAILED: {message}");
+        }
+    }
 
     private static void HookWithMessengerTestCode(WebViewMessenger messenger)
     {
@@ -102,46 +122,51 @@ partial class MainWindowViewModel
 
             var result_add = await messenger.InvokePeerMethodAsync<int>(
                 "js_add", 1, 2);
-            Trace.WriteLine($"js_add: {result_add}");
+            Assert(3, result_add, $"js_add: {result_add}");
+
             var result_sub = await messenger.InvokePeerMethodAsync<int>(
                 "js_sub", 1, 2);
-            Trace.WriteLine($"js_sub: {result_sub}");
+            Assert(-1, result_sub, $"js_sub: {result_sub}");
+
             var result_enum1 = await messenger.InvokePeerMethodAsync<ConsoleKey>(
                 "js_enum1", ConsoleKey.Print);
-            Trace.WriteLine($"js_enum1: {result_enum1}");
+            Assert(ConsoleKey.Print, result_enum1, $"js_enum1: {result_enum1}");
+
             var result_enum2 = await messenger.InvokePeerMethodAsync<ConsoleKey>(
                 "js_enum2", ConsoleKey.Print);
-            Trace.WriteLine($"js_enum2: {result_enum2}");
+            Assert((ConsoleKey)42, result_enum2, $"js_enum2: {result_enum2}");
+
             var result_array = await messenger.InvokePeerMethodAsync<ConsoleKey[]>(
                 "js_array", new[] { ConsoleKey.Print, ConsoleKey.Enter, ConsoleKey.Escape });
-            Trace.WriteLine($"js_array: [{string.Join(",", result_array)}]");
+            Assert(new[] { ConsoleKey.Print, ConsoleKey.Enter, ConsoleKey.Escape }, result_array, $"js_array: {result_array}");
+
             try
             {
                 await messenger.InvokePeerMethodAsync("unknown");
-                Trace.WriteLine("BUG detected. [unknown]");
+                Trace.Fail("FAILED: [unknown]");
             }
             catch (PeerInvocationException)
             {
-                Trace.WriteLine("PASS: Unknown function invoking [unknown]");
+                Trace.WriteLine("PASSED: Unknown function invoking [unknown]");
             }
 
             /////////////////////////////////////////////////////////
             // Test JavaScript --> .NET methods with callback
 
-            Func<int, int, Task<string>> callback = (a, b) =>
-                Task.FromResult($"{a}-{b}");
-            var result_callback = await messenger.InvokePeerMethodAsync<string>("js_callback", 1, 2, callback);
-            Trace.WriteLine($"js_callback: {result_callback}");
+            Func<int, int, Task<int>> callback = (a, b) =>
+                Task.FromResult(a + b);
+            var result_callback = await messenger.InvokePeerMethodAsync<int>("js_callback", 1, 2, callback);
+            Assert(3, result_callback, $"js_callback: {result_callback}");
             
-            Func<int, int, CancellationToken, Task<string>> callback2 = (a, b, ct) =>
-                Task.FromResult($"{a}-{b}");
-            var result_callback2 = await messenger.InvokePeerMethodAsync<string>("js_callback2", 1, 2, callback2);
-            Trace.WriteLine($"js_callback2: {result_callback2}");
+            Func<int, int, CancellationToken, Task<int>> callback2 = (a, b, ct) =>
+                Task.FromResult(a - b);
+            var result_callback2 = await messenger.InvokePeerMethodAsync<int>("js_callback2", 1, 2, callback2);
+            Assert(-1, result_callback2, $"js_callback2: {result_callback2}");
             
-            Func<int, int, CancellationToken, Task<string>> callback3 = (a, b, ct) =>
-                Task.FromResult($"{a}-{b}");
-            var result_callback3 = await messenger.InvokePeerMethodAsync<string>("js_callback3", 1, 2, callback3);
-            Trace.WriteLine($"js_callback3: {result_callback3}");
+            Func<int, int, CancellationToken, Task<int>> callback3 = (a, b, ct) =>
+                Task.FromResult(a * b);
+            var result_callback3 = await messenger.InvokePeerMethodAsync<int>("js_callback3", 1, 2, callback3);
+            Assert(2, result_callback3, $"js_callback3: {result_callback3}");
 
             Trace.WriteLine("ALL TEST IS DONE AT .NET SIDE.");
         };
@@ -149,6 +174,18 @@ partial class MainWindowViewModel
 
     private static void AddJavaScriptTestCode(StringBuilder script)
     {
+        script.AppendLine("function assert(expected, actual, name) {");
+        script.AppendLine("  if (expected == actual) {");
+        script.AppendLine("    console.log('PASSED: ' + name);");
+        script.AppendLine("  } else if (expected instanceof Array && actual instanceof Array &&");
+        script.AppendLine("    expected.length == actual.length &&");
+        script.AppendLine("    expected.every((v,i)=> v === actual[i])) {");
+        script.AppendLine("    console.log('PASSED: ' + name);");
+        script.AppendLine("  } else {");
+        script.AppendLine("    console.error('FAILED: ' + name + ': ' + expected + ' != ' + actual);");
+        script.AppendLine("  }");
+        script.AppendLine("};");
+
         /////////////////////////////////////////////////////////
         // ---- Added more JavaScript test code fragments:
         // You can verify on the developer tooling window,
@@ -170,28 +207,35 @@ partial class MainWindowViewModel
         
         // Low-level invoking with `invokeHostMethod()` and varies of common types.
         script.AppendLine("  const result_add = await invokeHostMethod('add', 1, 2);");
-        script.AppendLine("  console.log('add: ' + result_add);");
+        script.AppendLine("  assert(3, result_add, 'add');");
+
         script.AppendLine("  const result_sub = await invokeHostMethod('sub', 1, 2);");
-        script.AppendLine("  console.log('sub: ' + result_sub);");
+        script.AppendLine("  assert(-1, result_sub, 'sub');");
+
         script.AppendLine("  const result_enum1 = await invokeHostMethod('fromEnum', 'Print');");
-        script.AppendLine("  console.log('enum1: ' + result_enum1);");
+        script.AppendLine("  assert(42, result_enum1, 'enum1');");
+
         script.AppendLine("  const result_enum2 = await invokeHostMethod('fromEnum', 42);");
-        script.AppendLine("  console.log('enum2: ' + result_enum2);");
+        script.AppendLine("  assert(42, result_enum2, 'enum2');");
+
         script.AppendLine("  const result_enum3 = await invokeHostMethod('toEnum', 42);");
-        script.AppendLine("  console.log('enum3: ' + result_enum3);");
+        script.AppendLine("  assert('print', result_enum3, 'enum3');");
+
         script.AppendLine("  const result_array = await invokeHostMethod('array', [42, 13, 27]);");
-        script.AppendLine("  console.log('array: ' + result_array);");
-        script.AppendLine("  const result_callback = await invokeHostMethod('callback', 1, 2, async (a, b) => a + '-' + b);");
-        script.AppendLine("  console.log('callback: ' + result_callback);");
-        script.AppendLine("  const result_callback2 = await invokeHostMethod('callback2', 1, 2, async (a, b, ct) => a + '-' + b);");
-        script.AppendLine("  console.log('callback2: ' + result_callback2);");
+        script.AppendLine("  assert(['print','enter','escape'], result_array, 'array');");
+
+        script.AppendLine("  const result_callback = await invokeHostMethod('callback', 1, 2, async (a, b) => a - b);");
+        script.AppendLine("  assert(-1, result_callback, 'callback');");
+
+        script.AppendLine("  const result_callback2 = await invokeHostMethod('callback2', 1, 2, async (a, b, ct) => a + b);");
+        script.AppendLine("  assert(3, result_callback2, 'callback2');");
 
         // Unknown method with `invokeHostMethod()`.
         script.AppendLine("  try {");
         script.AppendLine("    await invokeHostMethod('unknown', 12, 34, 56);");
-        script.AppendLine("    console.log('BUG detected [unknown]');");
+        script.AppendLine("    console.error('FAILED: [unknown]');");
         script.AppendLine("  } catch (e) {");
-        script.AppendLine("    console.log('PASS: Unknown method invoking [unknown]');");
+        script.AppendLine("    console.log('PASSED: Unknown method invoking [unknown]');");
         script.AppendLine("  }");
 
 #if WINDOWS_FORMS
@@ -223,9 +267,9 @@ partial class MainWindowViewModel
         // Unknown method on a object.
         script.AppendLine("  try {");
         script.AppendLine("    await invokeHostMethod('calc.mult', 1, 2);");
-        script.AppendLine("    console.log('BUG detected [calc.mult]');");
+        script.AppendLine("    console.error('FAILED: [calc.mult]');");
         script.AppendLine("  } catch (e) {");
-        script.AppendLine("    console.log('PASS: Unknown method invoking [calc.mult]');");
+        script.AppendLine("    console.log('PASSED: Unknown method invoking [calc.mult]');");
         script.AppendLine("  }");
 
         // CancellationToken (obsoleted)
@@ -239,9 +283,9 @@ partial class MainWindowViewModel
         script.AppendLine("  ct2.cancel();");
         script.AppendLine("  try {");
         script.AppendLine("    await result_calc_add_cancellable2_p;");
-        script.AppendLine("    console.log('BUG detected [calc.add_cancellable2]');");
+        script.AppendLine("    console.error('FAILED: [calc.add_cancellable2]');");
         script.AppendLine("  } catch (e) {");
-        script.AppendLine("    console.log('PASS: Operation canceled [calc.add_cancellable2]');");
+        script.AppendLine("    console.log('PASSED: Operation canceled [calc.add_cancellable2]');");
         script.AppendLine("  }");
 
         // AbortController/AbortSignal
@@ -255,9 +299,9 @@ partial class MainWindowViewModel
         script.AppendLine("  ac2.abort();");
         script.AppendLine("  try {");
         script.AppendLine("    await result_calc_add_cancellable2_p_as;");
-        script.AppendLine("    console.log('BUG detected [calc.add_cancellable2_as]');");
+        script.AppendLine("    console.error('FAILED: [calc.add_cancellable2_as]');");
         script.AppendLine("  } catch (e) {");
-        script.AppendLine("    console.log('PASS: Operation canceled [calc.add_cancellable2_as]');");
+        script.AppendLine("    console.log('PASSED: Operation canceled [calc.add_cancellable2_as]');");
         script.AppendLine("  }");
 
         // AbortController/AbortSignal (nested)
@@ -271,9 +315,9 @@ partial class MainWindowViewModel
         script.AppendLine("  ac4.abort();");
         script.AppendLine("  try {");
         script.AppendLine("    await result_calc_nested_cancellable_p;");
-        script.AppendLine("    console.log('BUG detected [calc.nested_cancellable_p]');");
+        script.AppendLine("    console.error('FAILED: [calc.nested_cancellable_p]');");
         script.AppendLine("  } catch (e) {");
-        script.AppendLine("    console.log('PASS: Operation canceled [calc.nested_cancellable_p]');");
+        script.AppendLine("    console.log('PASSED: Operation canceled [calc.nested_cancellable_p]');");
         script.AppendLine("  }");
 
 #if WINDOWS_FORMS
@@ -298,9 +342,9 @@ partial class MainWindowViewModel
 
         script.AppendLine("  try {");
         script.AppendLine("    await calc.add_obsoleted3(1, 2);");
-        script.AppendLine("    console.log('BUG detected [calc.add_obsoleted3]');");
+        script.AppendLine("    console.error('FAILED: [calc.add_obsoleted3]');");
         script.AppendLine("  } catch (e) {");
-        script.AppendLine("    console.log('PASS: Fatal obsoleted [calc.add_obsoleted3]');");
+        script.AppendLine("    console.log('PASSED: Fatal obsoleted [calc.add_obsoleted3]');");
         script.AppendLine("  }");
 
         // Able to call different functions on a proxy object.
@@ -310,12 +354,12 @@ partial class MainWindowViewModel
         // Interoperated exceptions.
         script.AppendLine("  try {");
         script.AppendLine("    await calc.willBeThrow(1, 2);");
-        script.AppendLine("    console.log('BUG detected [calc.willBeThrow, 1]');");
+        script.AppendLine("    console.error('FAILED: [calc.willBeThrow, 1]');");
         script.AppendLine("  } catch (e) {");
         script.AppendLine("    if (e.props.a == 1 && e.props.b == 2)");
-        script.AppendLine("      console.log('PASS: Will be throw [calc.willBeThrow]');");
+        script.AppendLine("      console.log('PASSED: Will be throw [calc.willBeThrow]');");
         script.AppendLine("    else");
-        script.AppendLine("      console.log('BUG detected [calc.willBeThrow, 2]');");
+        script.AppendLine("      console.error('FAILED: [calc.willBeThrow, 2]');");
         script.AppendLine("  }");
 
         script.AppendLine("  console.log('ALL TEST IS DONE AT JavaScript SIDE.');");
